97baf30ccb864c9ff7cd7da038d44edfdadda6ab
ledok <supermaster.ua.lviv@gmail.com>
2026-04-29 13:39:07 +0200

    First commit

---
+++ src/main/java/net/ledok/mobs_ld/MobsLdMod.java
@@ -0,0 +1,10 @@
+package net.ledok.mobs_ld;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MobsLdMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/ledok/mobs_ld/client/MobsLdModClient.java
@@ -0,0 +1,10 @@
+package net.ledok.mobs_ld.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobsLdModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
